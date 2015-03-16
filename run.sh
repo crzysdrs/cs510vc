0497a826b2a88faeb19c00e7af9fd00a82a7b7e3
Mitch Souders <mitch.souders@gmail.com>
2015-03-15 22:34:28 -0700

    Wrote Report

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p out
 ./tracking.py annex/Walking.mkv --max_size=10000 --mask_shadow --output out/walk.avi
+./tracking.py annex/Walking.mkv --max_size=10000 --mask_shadow --output out/walk_debug.avi --debug
 ./tracking.py annex/Subway.mkv --max_size=6400 --output out/subway.avi
-
+./tracking.py annex/Subway.mkv --max_size=6400 --output out/subway_debug.avi --debug
